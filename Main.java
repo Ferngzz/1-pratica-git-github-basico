1c96f48c3a26de7a6f3871828795d7a84e9b5972
Vinicius Silva <vinicius14082004@gmail.com>
2024-08-23 21:59:53 -0300

    add - listaElementosRepetidos

--- Main.java
+++ Main.java
@@ -67,6 +67,20 @@ public class Main {
         return cont;   
     }
 
+    public List<Integer> listaElementosRepetidos(ArrayList<Integer> l){
+        List<Integer> elementosRepetidos = new ArrayList<>();
+
+        for(int i = 0 ; i < elementosRepetidos.size() ; i++){
+            for(int j = i + 1 ; j < elementosRepetidos.size() ; j++ ) {
+                if( i == j) {
+                    elementosRepetidos.add(l.get(i));
+                }
+            }
+        }
+
+        return elementosRepetidos;
+    }
+    
     public static void main(String[] args) {
 
         List<Integer> l1 = new ArrayList<>();
