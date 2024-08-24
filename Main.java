4c34d61d14ae0fff1c0ffa92e88dc3f38e033fb1
FERNANDO GAZZANA MACHADO DA SILVA <fernando.gazzana@edu.pucrs.br>
2024-08-23 21:32:13 -0300

    Union fix

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
 
         List<Integer> unionList = new ArrayList<>(l1);
 
-        for (int i : unionList){
+        for (int i = 0; i < l2.size(); i++){
             if(!unionList.contains(l2.get(i))){
-                unionList.add(i);
+                unionList.add(l2.get(i));
             }
         }
 
@@ -17,5 +17,28 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+
+        l1.add(1);
+        l1.add(2);
+        l1.add(3);
+        l1.add(4);
+        l1.add(5);
+        l1.add(6);
+
+        System.out.println("l1: " + l1);
+
+        l2.add(3);
+        l2.add(4);
+        l2.add(5);
+        l2.add(6);
+        l2.add(7);
+        l2.add(8);
+
+        System.out.println("l2: " + l2);
+        System.out.println("Union: " + union(l1,l2));
+
     }
 }
